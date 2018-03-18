c64547fb3e08791b315bf6495cfa36f9d7da2826
Or Keret <orkeret@gmail.com>
2018-03-18 16:32:52 +0000

    adding color to the text

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,16 +8,20 @@ LunchCheckController.$inject(['$scope']);
 function LunchCheckController($scope) {
   $scope.dishes = "";
   $scope.message = "";
+  $scope.messageColor = "green";
   $scope.processDishes = function() {
 
     if (!$scope.dishes) { // empty/undefined
       $scope.message = "Please enter data first";
+      $scope.messageColor = "red";
     } else { // there is at least one item
       var dishesAsArray = $scope.dishes.split(',');
       if (dishesAsArray.length <= 3) {
         $scope.message = "Enjoy!";
+        $scope.messageColor = "green";
       } else { // more than 3
         $scope.message = "Too much!";
+        $scope.messageColor = "green";
       }
     }
   };
