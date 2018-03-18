9537214881d2854eddd99a9e55666c51bcf55bca
Or Keret <orkeret@gmail.com>
2018-03-18 18:53:14 +0000

    module1 refactoring the filtering out bit

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,39 +9,28 @@ function LunchCheckController($scope) {
   $scope.dishes = "";
   $scope.message = "";
   $scope.messageColor = "";
-  $scope.processDishes = function() {
+  $scope.checkDishes = function() {
 
     if (!$scope.dishes) { // empty/undefined
       $scope.message = "Please enter data first";
       $scope.messageColor = "red";
-    } else { // arr isn't empty (but might have only commas)
-      var dishesAsArray = $scope.dishes.split(',');
-      var numOfItems = countNonEmptyItems(dishesAsArray);
+      return;
+    }
 
-      if (numOfItems === 0) { // only commas scenario, e.g.: ",,,,,,,"
-        $scope.message = "Please enter data first";
-        $scope.messageColor = "red";
-      } else if (numOfItems <= 3) {
-        $scope.message = "Enjoy!";
-        $scope.messageColor = "green";
-      } else { // more than 3
-        $scope.message = "Too much!";
-        $scope.messageColor = "green";
-      }
+    // arr isn't empty (but might have only commas)
+    var dishesAsArray = $scope.dishes.split(',');
+    var nonEmptyDishes = dishesAsArray.filter(d => d.trim());
+
+    if (nonEmptyDishes.length === 0) { // only commas scenario, e.g.: ",,,,,,,"
+      $scope.message = "Please enter data first";
+      $scope.messageColor = "red";
+    } else if (nonEmptyDishes.length <= 3) {
+      $scope.message = "Enjoy!";
+      $scope.messageColor = "green";
+    } else { // more than 3
+      $scope.message = "Too much!";
+      $scope.messageColor = "green";
+    }
     }
   };
-}
-
-// precondtion - arr.length >= 1
-function countNonEmptyItems(arr) {
-  var counter = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].trim()) {
-      counter++;
-    }
-  }
-
-  return counter;
-}
 })();
